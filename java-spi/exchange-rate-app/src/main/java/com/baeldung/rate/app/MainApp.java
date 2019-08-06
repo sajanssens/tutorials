8b163818c1ab28c3d449a771c838e857c454bfe5
bramjanssens <bram.janssens@infosupport.com>
2019-08-06 15:45:57 +0200

    Let maven execute MainApp.

--- java-spi/exchange-rate-app/src/main/java/com/baeldung/rate/app/MainApp.java
+++ java-spi/exchange-rate-app/src/main/java/com/baeldung/rate/app/MainApp.java
@@ -8,6 +8,8 @@ import java.util.List;
 
 public class MainApp {
     public static void main(String... args) {
+        System.out.println("Running MainApp...");
+
         ExchangeRate.providers().forEach(provider -> {
             System.out.println("Retreiving USD quotes from provider :" + provider);
             List<Quote> quotes = provider.create().getQuotes("USD", LocalDate.now());
